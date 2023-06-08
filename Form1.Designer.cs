b6674753b97e82e77ae83f247b707db0e495d16c
Sung whan Kim <62971846+lazaros7sungwhan@users.noreply.github.com>
2023-06-09 01:52:50 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace Daqmx_test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.textBox8);
+            this.groupBox1.Controls.Add(this.textBox7);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Font = new System.Drawing.Font("D2Coding", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox1.Location = new System.Drawing.Point(55, 174);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(738, 639);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.Color.Black;
+            this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox8.Font = new System.Drawing.Font("D2Coding ligature", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox8.ForeColor = System.Drawing.Color.Lime;
+            this.textBox8.Location = new System.Drawing.Point(390, 428);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(289, 98);
+            this.textBox8.TabIndex = 8;
+            this.textBox8.Text = "0";
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.Color.Black;
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox7.Font = new System.Drawing.Font("D2Coding ligature", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox7.ForeColor = System.Drawing.Color.White;
+            this.textBox7.Location = new System.Drawing.Point(42, 428);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(289, 98);
+            this.textBox7.TabIndex = 7;
+            this.textBox7.Text = "0";
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.Black;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Font = new System.Drawing.Font("D2Coding ligature", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox6.ForeColor = System.Drawing.Color.Lime;
+            this.textBox6.Location = new System.Drawing.Point(390, 298);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(289, 98);
+            this.textBox6.TabIndex = 6;
+            this.textBox6.Text = "0";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.Black;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Font = new System.Drawing.Font("D2Coding ligature", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox5.ForeColor = System.Drawing.Color.White;
+            this.textBox5.Location = new System.Drawing.Point(42, 298);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(289, 98);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.Text = "0";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Black;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("D2Coding ligature", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox4.ForeColor = System.Drawing.Color.Lime;
+            this.textBox4.Location = new System.Drawing.Point(390, 168);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(289, 98);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.Text = "0";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("D2Coding ligature", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox3.ForeColor = System.Drawing.Color.White;
+            this.textBox3.Location = new System.Drawing.Point(42, 168);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(289, 98);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "0";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.Font = new System.Drawing.Font("D2Coding", 10F);
+            this.button2.Location = new System.Drawing.Point(247, 567);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 59);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "값 적용";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("D2Coding ligature", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox2.ForeColor = System.Drawing.Color.Lime;
+            this.textBox2.Location = new System.Drawing.Point(390, 38);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(289, 98);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("D2Coding ligature", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(42, 38);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(289, 98);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Black;
+            this.comboBox1.Font = new System.Drawing.Font("D2Coding", 10F);
+            this.comboBox1.ForeColor = System.Drawing.Color.Snow;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(55, 79);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(272, 35);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.Black;
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox2.Font = new System.Drawing.Font("D2Coding", 10F);
+            this.comboBox2.ForeColor = System.Drawing.Color.Snow;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(339, 79);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(272, 35);
+            this.comboBox2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Font = new System.Drawing.Font("D2Coding", 10F);
+            this.button1.Location = new System.Drawing.Point(623, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "연결";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1,
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 874);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(848, 44);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 44);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(156, 38);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("D2Coding ligature", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(55, 129);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(166, 36);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "SAVE_FILE";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔스퀘어", 14.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(35, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(705, 38);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "입출력 채널 0으로 선택 뒤 반드시 끝에 :3 붙일 것 ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(848, 918);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Daqmx_AIAO";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NationalInstruments.DAQmx; //이거 하나만 있으면 Ni Daq 됨
+using System.Threading; // Threadsleep 및 쓰레드생성에 필요
+using System.IO; // 데이터 저장 (StreamReader 등에 필요)
+using System.Diagnostics; // StopWatch 에 필요함
+using System.Reflection.Emit; 
+
+namespace Daqmx_test1
+{
+    public partial class Form1 : Form
+    {
+        bool Save = false;
+        double voltageData0 = 0;
+        Stopwatch _sw;
+        bool Connect = false,reconnect=false;
+        
+        /******************************DATA******************************/
+        double[,] VI_Data; //Ni 함수에 넣을 2차원 배열
+        double[] VI_Save_Data = { 0, 0, 0, 0 }; //VI 용 실제 데이터 기록 변수
+        double[] AO_Data= { 0,0,0,0 }; // 배열 4개구성에 대해 0으로 초기화 4개 설정 시 Analog 채널선택은 반드시 4개 여야 함
+        /*****************************DAQMX****************************/
+        NationalInstruments.DAQmx.Task Task1; // AI 태스크 기본 
+
+        AnalogMultiChannelReader analogReader;
+        NationalInstruments.DAQmx.Task Task2; // AO 태스크 기본 
+        AnalogMultiChannelWriter analogWriter;
+
+        /*****************************CallBack 함수 선언****************************/
+        AsyncCallback analogCallback; //콜백함수(소환되는 함수)가 Ni Read 함수에 쓰임
+
+        /******************************Save 관련 Thread 선언 *****************************/
+        Thread saveThread;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.Items.AddRange(NationalInstruments.DAQmx.DaqSystem.Local.GetPhysicalChannels(PhysicalChannelTypes.AI, PhysicalChannelAccess.External)); //콤보박스에 AI 추가시키는 함수
+            comboBox2.Items.AddRange(NationalInstruments.DAQmx.DaqSystem.Local.GetPhysicalChannels(PhysicalChannelTypes.AO, PhysicalChannelAccess.External)); //콤보박스에 AO 추가시키는 함수
+            if(comboBox1.Items.Count > 0 )comboBox1.SelectedIndex = 0; // 여러개가 요소로 있을 경우 0번의 값을 기본으로 지정
+            if(comboBox2.Items.Count > 0 )comboBox2.SelectedIndex = 0; // 여러개가 요소로 있을 경우 0번의 값을 기본으로 지정
+            toolStripLabel1.Text = "Save : Not Allocated";
+            toolStripLabel1.ForeColor = Color.Red;
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Connect 
+        {
+            try
+            {
+
+                if (Connect == false) // 처음은 버튼이 눌려있지 않음
+                {
+                    /******************************DAQMX*****************************/
+                    Task1 = new NationalInstruments.DAQmx.Task(); // AI 태스크 기본 
+                    Task2 = new NationalInstruments.DAQmx.Task(); // AO 태스크 기본 
+
+                    //AI Configuration
+                    Task1.AIChannels.CreateVoltageChannel(comboBox1.Text, "", AITerminalConfiguration.Differential, -10, 10, AIVoltageUnits.Volts);
+                    Task1.Timing.ConfigureSampleClock("", 100, SampleClockActiveEdge.Rising, SampleQuantityMode.ContinuousSamples, 100);
+
+                    analogReader = new AnalogMultiChannelReader(Task1.Stream);
+                    analogReader.SynchronizeCallbacks = true;
+
+                    analogCallback = new AsyncCallback(callbackfunction_1);
+                    analogReader.BeginReadMultiSample(100, analogCallback, Task1);
+
+                    //AO Configuration
+                    Task2.AOChannels.CreateVoltageChannel(comboBox2.Text, "", -10, 10, AOVoltageUnits.Volts);
+                    analogWriter = new AnalogMultiChannelWriter(Task2.Stream);
+
+                    /*******************처음 시작 시 Voltage Out 은  반드시 0 에서 시작*************************/
+                    AO_Data[0] = Convert.ToDouble(textBox1.Text);
+                    AO_Data[1] = Convert.ToDouble(textBox3.Text);
+                    AO_Data[2] = Convert.ToDouble(textBox5.Text);
+                    AO_Data[3] = Convert.ToDouble(textBox7.Text);
+
+                    analogWriter.WriteSingleSample(true, AO_Data);
+                    /*******************************************************************************************/
+
+
+                    if (checkBox1.Checked != true)  // Save 체크박스가 해제된 채로 시작될 경우 방해를 받지 않도록 체크박스를 숨긴다. 
+
+                    {
+                        checkBox1.Visible = false;
+                    }
+                    else if (checkBox1.Checked == true) // Save 체크박스가 눌린 채로 시작될 경우 중복을 방지하기 위해 체크박스를 숨기고 저장 쓰레드를 시작한다. 
+                    {
+                        checkBox1.Visible = false;
+                        saveThread = new Thread(() => saveThreadFunction());
+                        saveThread.Start();
+                    }
+                    button1.Text = "연결해제"; // 버튼의 이름을 연결해제로 바꾼다. 
+                    Connect = true;
+                }
+
+                else if (Connect == true) // 이미 버튼이 눌려 있을 경우 연결 해제 할 수 있도록 연결해제 버튼으로 변환
+                {
+                    if (saveThread != null)
+                        saveThread.Abort();
+                    Task1.Dispose();
+                    Task2.Dispose();
+                    button1.Text = "연결";
+                    Connect=false;
+                    reconnect = true;
+                    checkBox1.Visible = true;
+                    toolStripLabel1.Text = "Save : Not Allocated";
+                    toolStripLabel1.ForeColor = Color.Red;
+                    checkBox1.Checked = false;
+                }
+            }
+            catch{MessageBox.Show("접속에 문제 발생..");}
+            
+        }
+        void callbackfunction_1(IAsyncResult ar)
+        {
+            try {
+                if (Connect == true)
+                {
+                    if ((Task1 != null) && (Task1 == ar.AsyncState))
+                        VI_Data = analogReader.EndReadMultiSample(ar); // 출력이 전압 값으로 나오는 함수임
+                    if (Task1 != null)
+                        analogReader.BeginReadMultiSample(100, analogCallback, Task1); // CallBack 부르는 Delegate 함수 또 부름 
+
+                    Invoke(new MethodInvoker(delegate () {
+
+                        textBox2.Text = VI_Data[0, 0].ToString("f3"); VI_Save_Data[0] = VI_Data[0, 0];
+                        textBox4.Text = VI_Data[1, 0].ToString("f3"); VI_Save_Data[1] = VI_Data[1, 0];
+                        textBox6.Text = VI_Data[2, 0].ToString("f3"); VI_Save_Data[2] = VI_Data[2, 0];
+                        textBox8.Text = VI_Data[3, 0].ToString("f3"); VI_Save_Data[3] = VI_Data[3, 0];
+                    }));
+                }
+            }
+            catch { MessageBox.Show("Callback 함수에 문제 발생.."); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)  // 이 버튼을 누를 경우 Analog Out 값이 적용된다.
+        {
+            try
+            {
+                AO_Data[0] = Convert.ToDouble(textBox1.Text);
+                AO_Data[1] = Convert.ToDouble(textBox3.Text);
+                AO_Data[2] = Convert.ToDouble(textBox5.Text);
+                AO_Data[3] = Convert.ToDouble(textBox7.Text);
+
+                analogWriter.WriteSingleSample(true, AO_Data);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("값 입력 실패 .. ");
+                throw;
+            }
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            if (checkBox1.Checked == true)
+            {
+                using (SaveFileDialog saveFileDialog1 = new SaveFileDialog())
+                {
+                    saveFileDialog1.Filter = "Dat_file.(*.dat)|*.dat";
+                    saveFileDialog1.FilterIndex = 1;
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        toolStripLabel1.Text = saveFileDialog1.FileName;
+                        toolStripLabel1.ForeColor= Color.Blue;
+                    }
+                    if (saveFileDialog1.FileName == "")
+                    {
+                        toolStripLabel1.Text = "Save : Not Allocated";
+                        toolStripLabel1.ForeColor = Color.Red;
+                        checkBox1.Checked = false;
+                    }
+                }
+            }
+            else
+            {
+                toolStripLabel1.Text = "Save : Not Allocated";
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(saveThread != null)
+            saveThread.Abort();
+        }
+        void saveThreadFunction()
+        {
+            try
+            {
+                _sw = new Stopwatch();
+                    _sw.Start();
+
+                    using (StreamWriter out_file = new StreamWriter(toolStripLabel1.Text))
+                    {
+                        out_file.WriteLine("Time\tData1\tData2\tData3\tData4");
+                        do
+                        {
+                            out_file.WriteLine((Convert.ToDouble(_sw.ElapsedMilliseconds) / 1000).ToString("f3") + "\t" + VI_Save_Data[0].ToString("f3") + "\t" + VI_Save_Data[1].ToString("f3") + "\t" + VI_Save_Data[2].ToString("f3") + "\t" + VI_Save_Data[3].ToString("f3"));
+                            Thread.Sleep(10);
+
+                        } while (true);
+                    }
+            }
+            catch (Exception) {  }
+        }
+    }
+}
